a97c96b81657ab165f5e0c49d7b508dc3ef0a1de
fabriciodc50-beep <fabriciodc50@gmail.com>
2026-07-28 20:50:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'lumiar-docs-v1';
+const ARQUIVOS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icone-192.png',
+  './icone-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARQUIVOS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      if (resp) {
+        fetch(e.request).then(r => {
+          if (r && r.status === 200) caches.open(CACHE).then(c => c.put(e.request, r.clone()));
+        }).catch(() => {});
+        return resp;
+      }
+      return fetch(e.request).then(r => {
+        if (r && r.status === 200 && e.request.url.startsWith(self.location.origin)) {
+          const copia = r.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copia));
+        }
+        return r;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
